bbb4acf46de839dda5e3695ca9a3dc1e94377aa9
801 Txchii <sdgaletech@gmail.com>
2023-01-26 15:22:07 -0700

    ?

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-  let count: number = 0
+  let count: number = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
+
+  const down = () => {
+    count -= 1;
+  };
 </script>
 
 <button on:click={increment}>
+  count is {count}
+</button>
+
+<button on:click={down}>
   count is {count}
 </button>
